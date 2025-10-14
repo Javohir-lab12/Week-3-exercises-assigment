6d528ccd09f4e2fecb3d17b1ade77778a930abc8
Javohir-lab12 <karimovjavohir0612@gmail.com>
2025-10-14 22:24:38 +0500

    Update week_3_exercise_3.py

--- week_3_exercise_3.py
+++ week_3_exercise_3.py
@@ -1,6 +1,7 @@
 print("--- Triangle Pattern Printer ---")
 desired_height = int(input("Enter the desired height of the triangle: "))
 for row_num in range (desired_height):
-    for col_num in range(row_num + 15):
+    for col_num in range(row_num + 1):
         print("* ", end="")
+
     print()
